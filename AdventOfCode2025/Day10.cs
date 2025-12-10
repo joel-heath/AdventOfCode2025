3b4c130c24804923cf0c8ac22c455696886b5f92
Joel Heath <joelheath@outlook.com>
2025-12-10 06:42:45 +0000

    (In theory) part 2 solved but again far too slow

--- AdventOfCode2025/Day10.cs
+++ AdventOfCode2025/Day10.cs
@@ -46,7 +46,7 @@ public class Day10 : IDay
     };
     public Dictionary<string, string> UnitTestsP2 { get; } = new()
     {
-        { "TestInput1", "ExpectedOutput1" }
+        { "[.##.] (3) (1,3) (2) (2,3) (0,2) (0,1) {3,5,4,7}\r\n[...#.] (0,2,3,4) (2,3) (0,4) (0,1,2) (1,2,3,4) {7,5,12,7,2}\r\n[.###.#] (0,1,2,3,4) (0,3,4) (0,1,2,4,5) (1,2) {10,11,11,5,10,5}", "33" }
     };
 
     private static ulong Encode(bool[] bits)
@@ -140,6 +140,73 @@ public class Day10 : IDay
         return min;
     }
 
+    private static bool JoltagesConfiguredCorrectly(int[] joltages, int[] reference)
+        => joltages.SequenceEqual(reference);
+
+    private static bool AnyJoltageTooLarge(int[] joltages, int[] reference)
+        => joltages.Zip(reference).Any(t => t.First > t.Second);
+
+    private static int[] PushButton(int[] joltages, int[] button)
+    {
+        int[] output = new int[joltages.Length];
+        Array.Copy(joltages, output, joltages.Length);
+
+        //for (int i = 0; i < button.Length; i++)
+        //    output[i] += button[i];
+
+        foreach (var index in button)
+            output[index]++;
+
+        return output;
+    }
+
+    private static int CalculateJoltageDifference(int[] joltages, int[] reference)
+        => joltages.Zip(reference).Sum(t => t.Second - t.First);
+
+    private static int Configure2(Machine machine)
+    {
+        Queue<(int[], int)> queue = [];
+        Dictionary<string, int> visited = [];
+
+        queue.Enqueue((new int[machine.JoltageRequirements.Length], 0));
+
+        int min = int.MaxValue;
+        while (queue.TryDequeue(out var data))
+        {
+            (int[] joltages, int depth) = data;
+
+            if (depth >= min || AnyJoltageTooLarge(joltages, machine.JoltageRequirements))
+                continue;
+
+            if (JoltagesConfiguredCorrectly(joltages, machine.JoltageRequirements))
+            {
+                min = depth;
+                continue;
+            }
+
+            string encoded = string.Join(',', joltages);
+
+            if (visited.TryGetValue(encoded, out int visitedDepth) && visitedDepth >= depth)
+                continue;
+
+            visited[encoded] = depth;
+
+            var buttons = machine.ButtonWiringSchematics
+                .Select(button =>
+                {
+                    int[] pushed = PushButton(joltages, button);
+                    int diff = CalculateJoltageDifference(pushed, machine.JoltageRequirements);
+                    return (pushed, diff);
+                })
+                .OrderBy(button => button.diff);
+
+            foreach (var (pushed, _) in buttons)
+                queue.Enqueue((pushed, depth + 1));
+        }
+
+        return min;
+    }
+
     public string SolvePart1(string input)
     {
         Machine[] machines = [..input.Lines().Select(line =>
@@ -164,6 +231,23 @@ public class Day10 : IDay
 
     public string SolvePart2(string input)
     {
-        return $"{string.Empty}";
+        Machine[] machines = [..input.Lines().Select(line =>
+        {
+            bool[] indicators = [..Regex.Match(line, @"\[([.#]+)\]")
+                .Groups.Values.ElementAt(1).Value
+                .Select(l => l == '#')];
+
+            int[][] schematics = [..Regex.Matches(line, @"\((\d+(,\d+)*)\)")
+                .Select(match => match.Groups.Values.ElementAt(1).Value)
+                .Select(thing => thing.Split(',').Select(int.Parse).ToArray())];
+
+            int[] requirements = [..Regex.Match(line, @"\{(\d+(,\d+)*)\}")
+                .Groups.Values.ElementAt(1).Value
+                .Split(',').Select(int.Parse)];
+
+            return new Machine(indicators, schematics, requirements);
+        })];
+
+        return $"{machines.Sum(Configure2)}";
     }
 }
