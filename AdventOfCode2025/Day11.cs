fb772d4ecf2635b9c9d438525b8d64bed45ab446
Joel Heath <joelheath@outlook.com>
2025-12-11 05:16:38 +0000

    Day 11 initial solution (BRO FINALLY AN EASY ONE)

--- AdventOfCode2025/Day11.cs
+++ AdventOfCode2025/Day11.cs
@@ -1,4 +1,5 @@
 using AdventOfCode2025.Utilities;
+using System.Text.RegularExpressions;
 
 namespace AdventOfCode2025;
 
@@ -7,22 +8,65 @@ public class Day11 : IDay
     public int Day => 11;
     public Dictionary<string, string> UnitTestsP1 { get; } = new() 
     {
-        { "TestInput1", "ExpectedOutput1" },
-        { "TestInput2", "ExpectedOutput2" }
+        { "aaa: you hhh\r\nyou: bbb ccc\r\nbbb: ddd eee\r\nccc: ddd eee fff\r\nddd: ggg\r\neee: out\r\nfff: out\r\nggg: out\r\nhhh: ccc fff iii\r\niii: out", "5" },
     };
     public Dictionary<string, string> UnitTestsP2 { get; } = new()
     {
-        { "TestInput1", "ExpectedOutput1" },
-        { "TestInput2", "ExpectedOutput2" }
+        { "svr: aaa bbb\r\naaa: fft\r\nfft: ccc\r\nbbb: tty\r\ntty: ccc\r\nccc: ddd eee\r\nddd: hub\r\nhub: fff\r\neee: dac\r\ndac: fff\r\nfff: ggg hhh\r\nggg: out\r\nhhh: out", "2" },
     };
+
+    private Dictionary<(int curr, int end), long> memo = [];
+
+    private long CountAllPaths(List<(string name, Dictionary<int, int> edges)> graph, int currentIndex, int endIndex)
+    {
+        if (currentIndex == endIndex)
+            return 1;
+
+        if (memo.TryGetValue((currentIndex, endIndex), out long cached))
+            return cached;
+
+        long paths = 0;
+
+        foreach ((int dest, int _) in graph[currentIndex].edges)
+            paths += CountAllPaths(graph, dest, endIndex);
+        
+        return memo[(currentIndex, endIndex)] = paths;
+    }
 
     public string SolvePart1(string input)
     {
-        return $"{string.Empty}";
+        var lines = input.Lines()
+            .Select(line => line.Split(": "))
+            .Select(parts => (source: parts[0], destinations: parts[1].Split(' ')))
+            .SelectMany(parts => parts.destinations.Select(d => (parts.source, destination: d, weight: 1)));
+
+        (List<(string name, Dictionary<int, int> edges)> graph, Dictionary<string, int> nameToIndexMap) = Utils.GenerateGraph(lines, directed: true);
+
+        int startIndex = nameToIndexMap["you"];
+        int endIndex = nameToIndexMap["out"];
+
+        return $"{CountAllPaths(graph, startIndex, endIndex)}";
     }
 
     public string SolvePart2(string input)
     {
-        return $"{string.Empty}";
+        var lines = input.Lines()
+            .Select(line => line.Split(": "))
+            .Select(parts => (source: parts[0], destinations: parts[1].Split(' ')))
+            .SelectMany(parts => parts.destinations.Select(d => (parts.source, destination: d, weight: 1)));
+
+        (List<(string name, Dictionary<int, int> edges)> graph, Dictionary<string, int> nameToIndexMap) = Utils.GenerateGraph(lines, directed: true);
+
+        long total =
+              CountAllPaths(graph, nameToIndexMap["svr"], nameToIndexMap["fft"])
+            * CountAllPaths(graph, nameToIndexMap["fft"], nameToIndexMap["dac"])
+            * CountAllPaths(graph, nameToIndexMap["dac"], nameToIndexMap["out"])
+            +
+              CountAllPaths(graph, nameToIndexMap["svr"], nameToIndexMap["dac"])
+            * CountAllPaths(graph, nameToIndexMap["dac"], nameToIndexMap["fft"])
+            * CountAllPaths(graph, nameToIndexMap["fft"], nameToIndexMap["out"]);
+
+
+        return $"{total}";
     }
 }
